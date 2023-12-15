f4683104c06bd01a7494751c10f13c5419bc8e96
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-12-15 14:56:30 +0200

    Trying to add form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from '.public/logo.png';
 import './App.css';
+import { Form, Input, Button} from "antd"
+import FormItem from 'antd/es/form/FormItem';
 
-function App() {
+ App = () => {
+  <div class="App">
   return (
-    <div className="App-main">
-      <header className="Cat-header">
-        <img src={logo} className="Cat-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="App-main"/>
+          <header className="Cat-header"/>
+              <img src={logo} className="Cat-logo" alt="logo"> </img>
+              <a
+                  className="App-link"
+                  href="https://podcat.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              /></a>
+              [ /** Ant Design Form component*/ ]
+              <Form>
+                  <Form.Item label="username" name="username"/>
+                  <Input placeholder="username"></Input>
+                  <Form.Item/>
+
+                  <Form.Item label="password" name="password"/>
+                    <Input placeholder="password"></Input>
+                  </Form.Item>
+
+                  <Form.Item/>
+                    <Button block type='primary' htmlType='submit'>Log In!</Button>
+                  </Form.Item>
+              </Form>
+              )
+          </header >
+      </div>
+  
+</div>
+
 
 export default App;
