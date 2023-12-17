f5c9c986fdeb24302fd418b0209706de225bab18
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-12-17 23:45:40 +0200

    yes

--- supabase/Client.js
+++ supabase/Client.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://sgqnuqznnzxwexqfdxyo.supabase.co"; 
-const supabaseKey =
+const supabaseAnonKey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNncW51cXpubnp4d2V4cWZkeHlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI4NDE5MDEsImV4cCI6MjAxODQxNzkwMX0.ksDjAI9BPpxhOfJmHNZIXveuU0Y8ALFsUBagyFgwiC8";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
